548840cb5e42ad4818c518aa4cfd1c0338c1ce24
Evlos <4tyle8@gmail.com>
2013-03-20 16:08:29 +0800

    cleaned

--- src/com/nisovin/shopkeepers/ShopListener.java
+++ src/com/nisovin/shopkeepers/ShopListener.java
@@ -9,8 +9,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
